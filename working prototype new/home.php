c721a9f88a3203a9bc6f97ca5c102799cd340fc4
sidh0050 <sattisidhu.kaur7@gmail.com>
2012-07-20 11:10:26 -0400

    Working prototype new with css.

---
+++ working prototype new/home.php
@@ -0,0 +1,126 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Web Application</title>
+<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="all">
+<div class="logo">
+<img src="images/logo.jpg" alt="Logo Image" >
+
+</div>
+<div class="img">
+<h1>Sidhuz Grocery List</h1>
+<div class="navigation">
+<nav>
+<ul>
+
+ <li><a href="home.php" style="text-decoration: none;">Home</a></li>
+<li><a href="lists.php" style="text-decoration: none;">Lists</a></li>
+<li><a href="recipies.php" style="text-decoration: none;">Recipes</a></li>
+<li><a href="contact.php" style="text-decoration: none;">Contact</a></li>
+
+</ul>
+</nav>
+</div>
+
+<div class="images">
+<img src="images/fruits.jpg" alt="Fruits Image" >
+<img src="images/grapes.jpg" alt="Grapes Image" >
+<img src="images/snacks.jpg" alt="Snacks Image" >
+</div>
+
+<div class="main">
+<h1>Fruits</h1>
+<p>The noun vegetable means an edible plant or part<br> 
+of a plan but usually excludes seeds and most sw<br>
+eet fruit.A snack is a portion of food oftentimes s<br>
+maller than that of a regular meal, that is generall<br>
+y eaten between meals.In botany, a fruit is a part<br>
+
+</p>
+
+
+<h1>Vegetables</h1>
+<p>n botany, a fruit is a part of a flowering plant that<br>
+derives from specific tissues of the flower, mainl<br>
+y one or more ovariesA snack is a portion of food<br>
+oftentimes smaller than that of a regular meal, t<br>
+be able to satisfy particular standards for complet<br>
+eness. 
+</p>
+
+<h1>Snacks</h1>
+<p>A snack is a portion of food oftentimes smaller th<br>
+an that of a regular meal, that is generally eaten <br>
+between meals.In botany, a fruit is a part of a flo<br>
+wering plant thatderives from specific tissues of <br>
+the flower, mainly one or more ovaries</p>
+</div>
+</div>
+<div class="sidebar">
+<h1>Recipies Books</h1>
+<ol>
+<li>Desserts By Koshi</li>
+<li>Snacks varieties by Alvira</li>
+<li>Rice recipies written by Satti sidhu</li>
+<li>Vegetarian Dishes.</li>
+<li>Non Vegetarian Recipies</li>
+<li>South Indian Recipies</li>
+<li>All Indian Dishes</li>
+<li>All Chennai Dishes</li>
+<li>All About Juices written By Satinder</li>
+<li>All about Paranthas</li>
+</ol>
+</div>
+
+
+
+
+<div class="footer">
+<div class="footerhome">
+
+<ul>
+<li><a href="home.php"style="text-decoration: none;">Home</a></li>
+<li>Make grocery list free</li>
+<li>Available items</li>
+<li>Information about items</li>
+</ul>
+
+</div>
+
+<div class="footerlists">
+
+<ul>
+<li><a href="lists.php"style="text-decoration: none;">Lists</a></li>
+<li>Grocery lists</li>
+<li>Different items list</li>
+<li>Available items list</li>
+<p>copyrightme@sattisidhu2012-2013</p>
+</ul>
+
+</div>
+
+<div class="footerrecipies">
+
+<ul>
+<li><a href="recipies.php"style="text-decoration: none;">Recipes</a></li>
+<li>Free recipes</li>
+<li>Free recipes books</li>
+<li>Recipes List</li>
+</ul>
+
+</div>
+
+
+
+
+</div>
+</div>
+
+
+</body>
+</html>
