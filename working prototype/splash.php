2664a37d3d6392308a7ff8a3a5358c8a4467fb77
sidh0050 <sattisidhu.kaur7@gmail.com>
2012-07-21 11:23:31 -0400

    Add css to files.

---
+++ working prototype/splash.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Web Application</title>
+<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="logo">
+<img src="images/logo.jpg" alt="Logo Image" >
+
+</div>
+
+
+
+<div class="img">
+<h1>Sidhuz Grocery List </h1>
+
+<div class="imagessplash">
+<img src="images/berries.jpg" alt="Berries Image" >
+<img src="images/redtomato.jpg" alt="Tomato Image" >
+<img src="images/strawberries.jpg" alt="Strawberries Image" >
+<img src="images/oranges.jpg" alt="Oranges Image" >
+<div class="splbtn">
+<button value="Home" onclick="location.href='home.php'">Home</button>		
+
+<button value="recipies"  onclick="location.href='recipies.php'">Recipes </button>
+<button value="lists"  onclick="location.href='lists.php'">lists </button>
+<button value="freebooks"  onclick="location.href='home.php'">freebooks </button>
+</div>
+</div>
+</div>
+
+
+
+</div>
+</body>
+</html>
